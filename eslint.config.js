4d386c4863b79c2f52d7e1b520da63fb5a947f7c
ryan-zayne <phoenixastro97@gmail.com>
2024-10-13 00:06:17 +0100

    feat: update eslint configuration - Add support for Tanstack Query - Update @zayne-labs/eslint-config to v0.2.0

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
-import zayne from "@zayne-labs/eslint-config";
+import { zayne } from "@zayne-labs/eslint-config";
 
 export default zayne({
-	ignores: ["dist/**", "eslint.config.js"],
+	ignores: ["dist/**"],
 	react: true,
 	tailwindcss: true,
+	tanstack: {
+		query: true,
+	},
 	typescript: {
 		tsconfigPath: "tsconfig.eslint.json",
 	},
