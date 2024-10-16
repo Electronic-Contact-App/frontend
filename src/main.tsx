c6edfad28c7ce00845ef38f50cadc1bd9d514207
ryan-zayne <phoenixastro97@gmail.com>
2024-10-16 22:07:49 +0100

    feat: wrap App component in React.StrictMode - Wrap the App component in React.StrictMode to enable additional checks and warnings for better development experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./tailwind.css";
 
-createRoot(document.querySelector("#root") as HTMLElement).render(<App />);
+createRoot(document.querySelector("#root") as HTMLElement).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
